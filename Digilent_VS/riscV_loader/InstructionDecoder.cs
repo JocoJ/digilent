462a16fe08b674f605d2e0721f331bd7423a4480
Cristy <cristy@nospam.org>
2019-03-29 15:39:58 +0200

    Added instruction decoding to the UI

---
+++ Digilent_VS/riscV_loader/InstructionDecoder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace riscV_loader
+{
+    class InstructionDecoder
+    {
+        public static string DecodeInstruction(UInt32 opcode)
+        {
+            return "";
+        }
+    }
+}
