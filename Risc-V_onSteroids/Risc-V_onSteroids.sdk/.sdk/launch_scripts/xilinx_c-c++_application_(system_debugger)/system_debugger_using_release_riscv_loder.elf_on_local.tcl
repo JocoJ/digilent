fdbf4c7272dc39961798a58a6118e20f27f8dc29
Cristy <cristy@nospam.org>
2019-04-08 12:52:33 +0300

    Added initial design of the CHM to the UI

--- Risc-V_onSteroids/Risc-V_onSteroids.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_release_riscv_loder.elf_on_local.tcl
+++ Risc-V_onSteroids/Risc-V_onSteroids.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_release_riscv_loder.elf_on_local.tcl
@@ -1,7 +1,7 @@
 connect -url tcp:127.0.0.1:3121
-source D:/Programare/Digilent/Risc-V_onSteroids/Risc-V_onSteroids.sdk/design_1_wrapper_hw_platform_0/ps7_init.tcl
+source D:/Altele/Digilent/Workspace/Risc-V_onSteroids/Risc-V_onSteroids.sdk/design_1_wrapper_hw_platform_0/ps7_init.tcl
 targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent Cora Z7 - 7007S 210370A92DAEA"} -index 0
-loadhw -hw D:/Programare/Digilent/Risc-V_onSteroids/Risc-V_onSteroids.sdk/design_1_wrapper_hw_platform_0/system.hdf -mem-ranges [list {0x40000000 0xbfffffff}]
+loadhw -hw D:/Altele/Digilent/Workspace/Risc-V_onSteroids/Risc-V_onSteroids.sdk/design_1_wrapper_hw_platform_0/system.hdf -mem-ranges [list {0x40000000 0xbfffffff}]
 configparams force-mem-access 1
 targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent Cora Z7 - 7007S 210370A92DAEA"} -index 0
 stop
@@ -10,7 +10,7 @@ ps7_post_config
 targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Cora Z7 - 7007S 210370A92DAEA"} -index 0
 rst -processor
 targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Cora Z7 - 7007S 210370A92DAEA"} -index 0
-dow D:/Programare/Digilent/Risc-V_onSteroids/Risc-V_onSteroids.sdk/riscV_loder/Release/riscV_loder.elf
+dow D:/Altele/Digilent/Workspace/Risc-V_onSteroids/Risc-V_onSteroids.sdk/riscV_loder/Release/riscV_loder.elf
 configparams force-mem-access 0
 targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Cora Z7 - 7007S 210370A92DAEA"} -index 0
 con
